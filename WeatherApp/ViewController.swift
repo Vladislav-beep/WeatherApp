98f547f8191bdf928409c56636fe09520b750382
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-10-13 20:08:54 +0300

    alert controller

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController {
+    func showAlert(title: String, message: String, completion: @escaping (String) -> (Void)) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addTextField { textField in
+            let cities = ["London", "Stambul"]
+            textField.placeholder = cities.randomElement()
+        }
+        
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = alert.textFields?.first
+            guard let city = textField?.text else { return }
+            
+            if city != "" {
+                let city = city.split(separator: " ").joined(separator: "%20")
+                completion(city)
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(search)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
 }
 
